5641b9299dd4eedbd4e0dda762299212239e9dfb
edgar <edgar.ochoa@devoteam.com>
2023-06-22 08:58:06 +0200

    clean up

--- includes/macro_convert_na_to_null.js
+++
@@ -1,9 +0,0 @@
-function macro_convert_na_to_null(field) {
-    return `
-    CASE ${field}
-        WHEN "NA" THEN NULL
-        ELSE ${field}
-        END AS ${field}
-    `;
-}
-module.exports = { macro_convert_na_to_null };
--- includes/macro_convert_null_to_na.js
+++
@@ -1,4 +0,0 @@
-function macro_convert_na_to_null(field) {
-    return `COALESCE(${field}, "NA")`;
-}
-module.exports = { macro_convert_null_to_na };
